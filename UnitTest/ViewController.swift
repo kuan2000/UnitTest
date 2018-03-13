426212aa8885a93f0d4bc02fb0bc3e06c07e6abc
kuan <khimyoong.kuan@icarasia.com>
2018-03-13 15:18:02 +0800

    Initial Commit

---
+++ UnitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UnitTest
+//
+//  Created by Kuan on 13/03/2018.
+//  Copyright © 2018 Kuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
